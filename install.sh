3e1060d52b2318bd240faeb2b0f34860b1845119
David Bernheisel <david@bernheisel.com>
2018-03-05 20:07:49 -0500

    Fix fancy_echo

--- install.sh
+++ install.sh
@@ -115,7 +115,7 @@ fi
 if is_linux; then
   column
   fancy_echo "Installing essential source-building packages" "$yellow"
-  fancy_echo "This may ask for sudo access for installs..."
+  fancy_echo "This may ask for sudo access for installs..." "$yellow"
 
   if is_ubuntu && ! grep "^[^#;]" Aptfile | sort -u | xargs sudo apt-get install -y; then
     fancy_echo "Could not install system utilities. Please install those and then re-run this script" "$red"
